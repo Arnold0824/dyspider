e1b4dc0e4130640853973382f567962245f571ea
Arnold Lee <418586403@qq.com>
2017-03-24 15:04:02 +0800

    sss

--- dailyJob.py
+++ dailyJob.py
@@ -1,4 +1,5 @@
 from datetime import date, time, datetime, timedelta
+from time import sleep
 import sys
 import os
 
@@ -14,44 +15,45 @@ def work(i):
     elif i==5:
         os.system('nohup scrapy crawl bttv >bttv.out &')
 
-def runTask(day=0, hour=0, min=0, second=0):
+def runTask(day=0, hour=0, minx=0, second=0):
     # Init time
-    now = datetime.now()
-    strnow = now.strftime('%Y-%m-%d %H:%M')
-    print("now:",strnow)
+    # now = datetime.now()
+    # strnow = now.strftime('%Y-%m-%d %H:%M')
+    # print("now:",strnow)
     # First next run time
-    period = timedelta(days=day, hours=hour, minutes=min, seconds=second)
-    next_time = now + period
-    strnext_time = next_time.strftime('%Y-%m-%d %H:%M')
-    print("下一次执行:",strnext_time)
+    # period = timedelta(days=day, hours=hour, minutes=min, seconds=second)
+    # next_time = now + period
+    # strnext_time = next_time.strftime('%Y-%m-%d %H:%M')
+    # print("下一次执行:",strnext_time)
     work(4)
     x=1
     while True:
         # Get system current time
 
-        iter_now = datetime.now()
-        iter_now_time = iter_now.strftime('%Y-%m-%d %H:%M')
-        if str(iter_now_time) == str(strnext_time):
+        # iter_now = datetime.now()
+        # iter_now_time = iter_now.strftime('%Y-%m-%d %H:%M')
+        sleep(second+minx*60+hour*60*60)
+        # if str(iter_now_time) == str(strnext_time):
             # Get every start work time
-            print("start work: %s" % iter_now_time)
-            a=datetime.now()
-            try:
-            # Call task func
-                work(x)
-            except:
-                pass
-            b=datetime.now()
-            print("job done")
-            dura=b-a    
-            # Get next iteration time
-            iter_time = iter_now + period
-            strnext_time = iter_time.strftime('%Y-%m-%d %H:%M')
-            print("next : %s" % strnext_time)
-            x+=1
-            if x>5:
-                x=1
-            # Continue next iteration
-            continue
+        # print("start work: %s" % iter_now_time)
+        # a=datetime.now()
+        try:
+        # Call task func
+            work(x)
+        except:
+            pass
+        # b=datetime.now()
+        print("job done")
+        # dura=b-a
+        # Get next iteration time
+        # iter_time = iter_now + period
+        # strnext_time = iter_time.strftime('%Y-%m-%d %H:%M')
+        # print("next : %s" % strnext_time)
+        x+=1
+        if x>5:
+            x=1
+        # Continue next iteration
+        continue
 #work()
 
-runTask(day=0, hour=4, min=0)
+runTask(day=0, hour=4, minx=0)
