020974c308b2af71e5f02ca7c2e41be3b5e99554
Ubuntu <ubuntu@ip-172-31-22-218.ap-northeast-1.compute.internal>
2017-04-27 02:54:57 +0000

    ss

---
+++ dy.sh
@@ -0,0 +1,7 @@
+cd /home/ubuntu/dyspider/
+scrapy crawl dy > dy.out &
+
+#nohup scrapy crawl kanxi >kanxi.out &
+#nohup scrapy crawl kanxitv >kanxitv.out &
+#nohup scrapy crawl bt >bt.out &
+#nohup scrapy crawl bttv >bttv.out &
---
+++ kanxi.sh
@@ -0,0 +1,6 @@
+cd /home/ubuntu/dyspider/
+#nohup scrapy crawl dy >dy.out &
+nohup scrapy crawl kanxi >kanxi.out &
+#nohup scrapy crawl kanxitv >kanxitv.out &
+#nohup scrapy crawl bt >bt.out &
+#nohup scrapy crawl bttv >bttv.out &
